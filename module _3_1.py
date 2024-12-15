9dded214bfb84a3e515782cce951c7c592bb2599
androsov96 <androsovaleksandrviktorovich@gmail.com>
2024-12-15 16:12:57 +0700

    исправление условия If

--- module _3_1.py
+++ module _3_1.py
@@ -11,12 +11,12 @@ print(string_info('ArmaGeDdoN'))
 
 def is_contains(string, list_to_search):
     count_call()
-    if isinstance(string, str):
+    if string.lower() in (i.lower() for i in list_to_search):
         return True
     else:
         return False
-print(is_contains({'hello world'}, ['hello', 'world']))
-print(is_contains('Hello world', [3,34,5,3]))
+print(is_contains('hello', ['heLlo', 'HeLlO', 'WoRlD']))
+print(is_contains('Hello world', ['HeLlO', 'WoRld', 'UrBaN']))
 print(call)
 
 
